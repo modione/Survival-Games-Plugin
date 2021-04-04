a83dbbf6406bbaf45ff63438c3c83a116aac410c
someron <70521849+someron-bot@users.noreply.github.com>
2021-04-04 18:10:16 +0200

    removed bug where leaving players still were in the game.

--- src/main/java/me/modione/sgplugin/listener/Listener.java
+++ src/main/java/me/modione/sgplugin/listener/Listener.java
@@ -1,11 +1,14 @@
 package me.modione.sgplugin.listener;
 
+import static me.modione.sgplugin.listener.onGameStart.playersig;
+
 import me.modione.sgplugin.SGPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Listener implements org.bukkit.event.Listener {
     @EventHandler
@@ -16,5 +19,9 @@ public class Listener implements org.bukkit.event.Listener {
             if (player.isOp()) return;
             player.kickPlayer(ChatColor.RED + "The Game is full!");
         }
+    }
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        if (onGameStart.gamestarted&&playersig.contains(event.getPlayer())) playersig.remove(event.getPlayer());
     }
 }
